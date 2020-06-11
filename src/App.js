8aaad46830979ac45231b8c7563f34840ac939a1
Damaris Goebel <damaris.goebel@gmx.de>
2020-06-11 20:11:44 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import ProjectDetails from './components/ProjectDetails';
+import {projects as Projects} from './components/Projects';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+
+class App extends Component {
+
+  state = {
+    loggedIn:true
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar></Navbar>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/projects' component={Projects}/>
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+
+
+          <Route exact path='/about' 
+          render={() => (
+ this.state.loggedIn ? (
+    <About/>
+  ) : (
+    <Redirect to ="/"></Redirect>
+  )
+)} 
+          // component={About} 
+
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
